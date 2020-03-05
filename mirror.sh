c5c359c58c63f91ab1cb016ef13b572c446642ff
Benjamin Smedberg <bsmedberg@xometry.com>
2020-03-05 17:35:22 -0500

    Push/create/delete handling

--- mirror.sh
+++ mirror.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-echo "env"
-env
-echo "done env"
-
 git config --global credential.username $GITLAB_USERNAME
 git config --global credential.helper cache
 git remote add mirror ${GITLAB_REPOSITORY}
 
-echo "Failing on purpose"
-exit 1
+if test "$GITHUB_EVENT_NAME" == "push" -o "$GITHUB_EVENT_NAME" == "create"; then
+  git push mirror ${GITHUB_REF}
+elif test "$GITHUB_EVENT_NAME" == "delete"; then
+  git push mirror :${GITHUB_REF}
+else
+  echo "Got unexpected GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
+  exit 1
+fi
+
+echo "Done"
