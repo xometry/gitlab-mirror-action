a4674450d6adbd57ef721fd28d9e372a633576d0
billhammond-dev <bill.hammond@xometry.com>
2023-10-19 16:13:06 -0500

    git issue?

--- mirror.sh
+++ mirror.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 git config --global credential.username $GITLAB_USERNAME
 git config --global core.askPass /get-password.sh
 git config --global credential.helper cache
+git config --global --add safe.directory /github/workspace
 
 if test "$GITHUB_EVENT_NAME" == "create"; then
     # Do nothing. Every "create" event *also* publishes a *push* event, even tags/branches created from github UI.
